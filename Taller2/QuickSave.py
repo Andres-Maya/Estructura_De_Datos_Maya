dac272b9bba0899233bc374bc6c6195d99d1d0a1
Andres-Maya <andrescamilomaya07@gmail.com>
2026-02-26 19:54:39 -0500

    Ahí ya

--- Taller2/QuickSave.py
+++
@@ -1,7 +0,0 @@
-class Ship:
-    def __init__(self, name: str, country: str):
-        self.name = name
-        self.country = country
-
-    def __str__(self):
-        return f"Ship: {self.name} [{self.country}]"
